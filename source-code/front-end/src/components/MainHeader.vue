e1c7558c268ac9c224628141d63ff19086f83e1e
bathanhtran <bathanhtran@gmail.com>
2023-10-31 00:02:08 +0700

    chinh sua

--- source-code/front-end/src/components/MainHeader.vue
+++ source-code/front-end/src/components/MainHeader.vue
@@ -7,7 +7,7 @@
             <router-link class="header-link" to="/about">
                 About
             </router-link>
-            <a class="header-link" target="_blank" href="https://github.com/chienbg2000/chienbg2000.github.io">
+            <a class="header-link" target="_blank" href="https://github.com/bathanhtran/bathanhtran.github.io">
                 Link tải mã nguồn
             </a>
         </div>
